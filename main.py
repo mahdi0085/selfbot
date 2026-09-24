88287b43c574e73fd8e6ae7a9323b931c10abfae
mahdi0085 <207360351+mahdi0085@users.noreply.github.com>
2026-09-24 16:46:44 +0330

    Add ping command

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 
 API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 
 client = TelegramClient("/data/selfbot", API_ID, API_HASH)
+
+
+@client.on(events.NewMessage)
+async def new_message(event):
+    print("New message received:")
+    print(event.raw_text)
+
+    if event.raw_text == "/ping":
+        await event.reply("Pong 🟢")
+
 
 async def main():
     print("Self-bot is starting...")
@@ -13,6 +23,7 @@ async def main():
 
     print(f"Logged in as: {me.first_name}")
     print("Self-bot is running...")
+
 
 with client:
     client.loop.run_until_complete(main())
